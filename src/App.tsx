b99b88c8339a6679b426fe6d3ddf44a4ea0fd19d
wwr <me@wwr.icu>
2025-01-03 22:02:17 +0800

    [feat] secondary route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {Layout} from "antd";
 import Sider from "antd/es/layout/Sider";
 import {Content} from "antd/es/layout/layout";
 import AdminMenu from "./components/AdminMenu.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Outlet, Route, Routes} from "react-router-dom";
 import {PostStatusEnum, TagTypeEnum} from "./model/enum.ts";
 import './App.css'
 import React, {Suspense} from "react";
@@ -10,48 +10,62 @@ import React, {Suspense} from "react";
 const LazyPostTable = React.lazy(() => import("./components/PostTable"));
 const LazyTagTable = React.lazy(() => import("./components/TagTable"));
 const LazyEditor = React.lazy(() => import("./components/Editor"));
+const LazyLoginPage = React.lazy(() => import("./components/LoginPage.tsx"));
+
+
+const AppLayout = <Layout style={{minHeight: '100vh', width: '100vw'}}>
+    <Layout>
+        <Sider theme='light'>
+            <AdminMenu/>
+        </Sider>
+        <Content style={{padding: '24'}}>
+            <Outlet/>
+        </Content>
+    </Layout>
+</Layout>
+
+const Loading = <div>Loading...</div>
 
 function App() {
-  return (
-      <BrowserRouter>
-      <Layout style={{minHeight: '100vh', width: '100vw'}}>
-          <Layout>
-              <Sider theme='light'>
-                  <AdminMenu/>
-              </Sider>
-              <Content style={{padding: '24'}}>
-                  <Routes>
-                      <Route path="/post" element={(
-                          <Suspense fallback={<div>Loading...</div>}>
+    return (
+        <Router>
+            <Routes>
+
+                <Route path="/login" element={(
+                    <Suspense fallback={Loading}>
+                        <LazyLoginPage/>
+                    </Suspense>
+                )}/>
+                <Route path='/' element={AppLayout}>
+                    <Route path="/post" element={(
+                        <Suspense fallback={Loading}>
                             <LazyPostTable postStatus={PostStatusEnum.PUBLISHED}/>
-                          </Suspense>
-                      )}/>
-                      <Route path="/draft" element={(
-                          <Suspense fallback={<div>Loading...</div>}>
-                              <LazyPostTable postStatus={PostStatusEnum.DRAFT}/>
-                          </Suspense>
-                      )}/>
-                      <Route path="/category" element={(
-                          <Suspense fallback={<div>Loading...</div>}>
-                              <LazyTagTable tagType={TagTypeEnum.POST_CAT}/>
-                          </Suspense>
-                      )}/>
-                      <Route path="/tag" element={(
-                          <Suspense fallback={<div>Loading...</div>}>
-                              <LazyTagTable tagType={TagTypeEnum.POST_TAG}/>
-                          </Suspense>
-                      )}/>
-                      <Route path="/edit/:id" element={(
-                          <Suspense fallback={<div>Loading...</div>}>
-                              <LazyEditor/>
-                          </Suspense>
-                      )}/>
-                  </Routes>
-              </Content>
-          </Layout>
-      </Layout>
-      </BrowserRouter>
-  )
+                        </Suspense>
+                    )}/>
+                    <Route path="/draft" element={(
+                        <Suspense fallback={Loading}>
+                            <LazyPostTable postStatus={PostStatusEnum.DRAFT}/>
+                        </Suspense>
+                    )}/>
+                    <Route path="/category" element={(
+                        <Suspense fallback={Loading}>
+                            <LazyTagTable tagType={TagTypeEnum.POST_CAT}/>
+                        </Suspense>
+                    )}/>
+                    <Route path="/tag" element={(
+                        <Suspense fallback={Loading}>
+                            <LazyTagTable tagType={TagTypeEnum.POST_TAG}/>
+                        </Suspense>
+                    )}/>
+                    <Route path="/edit/:id" element={(
+                        <Suspense fallback={Loading}>
+                            <LazyEditor/>
+                        </Suspense>
+                    )}/>
+                </Route>
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,11 @@
+import {LoginFormPage} from "@ant-design/pro-components";
+import React from "react";
+
+
+const LoginPage: React.FC = () => {
+    return (
+        <LoginFormPage />
+    )
+}
+
+export default LoginPage
