e79920f3f90a132c7c114a75812d3a0fe8f0b2b7
wwr <me@wwr.icu>
2025-02-22 22:16:55 +0800

    [feat] update post table style

--- src/components/AdminMenu.tsx
+++ src/components/AdminMenu.tsx
@@ -7,20 +7,17 @@ import {
     TagsOutlined,
     MenuOutlined,
 } from '@ant-design/icons'
-import {Button, Divider, Menu} from 'antd'
-import {createPostAPI} from "../api/post"
-import {PostDetailVO} from "../model/response"
-import {Link, useNavigate} from "react-router-dom"
+import {Divider, Menu} from 'antd'
+import {Link} from "react-router-dom"
 
 
 const AdminMenu: React.FC = () => {
-    const navigate = useNavigate()
     return (
         <>
             <Menu
                 mode="inline"
                 defaultSelectedKeys={['1']}
-                theme="light"
+                theme='light'
                 items={[
                     {
                         key: '1',
@@ -50,15 +47,7 @@ const AdminMenu: React.FC = () => {
                 ]}
             />
             <Divider/>
-            <Button
-                style={{width: '100%', marginTop: 10}}
-                onClick={() => {createPostAPI().then((postDetailVO: PostDetailVO) => {
-                    navigate(`/edit/${postDetailVO.id}`)
-                })}}
-            >New Post</Button>
         </>
-
-
     )
 }
 
--- src/components/PostTable.tsx
+++ src/components/PostTable.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import React, {useEffect, useState} from 'react'
-import {Popconfirm, Space, Table, Tag, Typography} from 'antd'
-import {deletePostAPI, getAllPost, updatePostStatusDetailAPI} from '../api/post'
+import {Button, Popconfirm, Space, Table, Tag, Typography} from 'antd'
+import {createPostAPI, deletePostAPI, getAllPost, updatePostStatusDetailAPI} from '../api/post'
 import {PostDetailPageVO, PostDetailVO, TagVO} from '../model/response'
 import {PostStatusEnum} from '../model/enum'
+import {useNavigate} from "react-router-dom";
+import {PlusOutlined} from "@ant-design/icons";
 
 const {Column} = Table
 
@@ -13,6 +15,7 @@ interface PostTableProps {
 }
 
 const AdminPostPage: React.FC<PostTableProps> = ({postStatus}: PostTableProps) => {
+    const navigate = useNavigate()
     const [list, setList] = useState<PostDetailVO[]>([])
     const [count, setCount] = useState<number>()
     const [pageIndex, setPageIndex] = useState<number>(1)
@@ -44,85 +47,95 @@ const AdminPostPage: React.FC<PostTableProps> = ({postStatus}: PostTableProps) =
     }, [pageSize, postStatus])
 
     return (
-        <Table<PostDetailVO>
-            style={{margin: 4}}
-            dataSource={list}
-            rowKey={(postDetailVO: PostDetailVO) => postDetailVO.id}
-            pagination={paginationProps}
-        >
-            <Column
-                <PostDetailVO>
-                title='Title'
-                dataIndex='title'
-                key='title'
-            />
-            <Column
-                <PostDetailVO>
-                title='Created At'
-                dataIndex='create_time'
-                key='create_time'
-                render={(_, {create_time}: PostDetailVO) => create_time.slice(0, 10)}
-            />
-            <Column
-                <PostDetailVO>
-                title='Category'
-                dataIndex='category'
-                key='category'
-                render={(_, {category}: PostDetailVO) => (
-                    <Tag color={'blue'}>
-                        {category?.name.toUpperCase()}
-                    </Tag>
-                )}
-            />
-            <Column
-                <PostDetailVO>
-                title='Tags'
-                dataIndex='tag_list'
-                key='tag_list'
-                render={(_, {tag_list}: PostDetailVO) => (
-                    <>
-                        {tag_list.map((tag: TagVO) => {
-                            return (
-                                <Tag color={'blue'} key={tag.id}>
-                                    {tag.name.toUpperCase()}
-                                </Tag>
-                            )
-                        })}
-                    </>
-                )}
-            />
-            <Column
-                <PostDetailVO>
-                title='Preview'
-                dataIndex='preview'
-                key='preview'
-            />
-            <Column
-                <PostDetailVO>
-                title='Action'
-                key='action'
-                render={(_, postDetailVO: PostDetailVO) => (
-                    <Space size='middle'>
-                        <Typography.Link href={`${import.meta.env.BASE_URL}/edit/${postDetailVO.id}`}>
-                            Edit
-                        </Typography.Link>
-                        <Popconfirm
-                            title={postDetailVO.status === 'published' ? 'Revoke' : 'Publish'}
-                            onConfirm={() => updatePostStatusDetailAPI(postDetailVO.id, postDetailVO.status === 'published' ? PostStatusEnum.DRAFT : PostStatusEnum.PUBLISHED)}
-                        >
-                            <Typography.Link color='red'>
-                                {postDetailVO.status === 'published' ? 'Revoke' : 'Publish'}
+        <div>
+            <Button
+                style={{marginTop: 4, marginLeft: 4}} type='primary'
+                onClick={() => {createPostAPI().then((postDetailVO: PostDetailVO) => {
+                    navigate(`/edit/${postDetailVO.id}`)
+                })}}
+            >
+                <PlusOutlined/>New Post
+            </Button>
+            <Table<PostDetailVO>
+                style={{margin: 4}}
+                dataSource={list}
+                rowKey={(postDetailVO: PostDetailVO) => postDetailVO.id}
+                pagination={paginationProps}
+            >
+                <Column
+                    <PostDetailVO>
+                    title='Title'
+                    dataIndex='title'
+                    key='title'
+                />
+                <Column
+                    <PostDetailVO>
+                    title='Created At'
+                    dataIndex='create_time'
+                    key='create_time'
+                    render={(_, {create_time}: PostDetailVO) => create_time.slice(0, 10)}
+                />
+                <Column
+                    <PostDetailVO>
+                    title='Category'
+                    dataIndex='category'
+                    key='category'
+                    render={(_, {category}: PostDetailVO) => (
+                        <Tag color={'blue'}>
+                            {category?.name.toUpperCase()}
+                        </Tag>
+                    )}
+                />
+                <Column
+                    <PostDetailVO>
+                    title='Tags'
+                    dataIndex='tag_list'
+                    key='tag_list'
+                    render={(_, {tag_list}: PostDetailVO) => (
+                        <>
+                            {tag_list.map((tag: TagVO) => {
+                                return (
+                                    <Tag color={'blue'} key={tag.id}>
+                                        {tag.name.toUpperCase()}
+                                    </Tag>
+                                )
+                            })}
+                        </>
+                    )}
+                />
+                <Column
+                    <PostDetailVO>
+                    title='Preview'
+                    dataIndex='preview'
+                    key='preview'
+                />
+                <Column
+                    <PostDetailVO>
+                    title='Action'
+                    key='action'
+                    render={(_, postDetailVO: PostDetailVO) => (
+                        <Space size='middle'>
+                            <Typography.Link href={`${import.meta.env.BASE_URL}/edit/${postDetailVO.id}`}>
+                                Edit
                             </Typography.Link>
-                        </Popconfirm>
-                        <Popconfirm title="Sure to Delete?" onConfirm={() => deletePostAPI(postDetailVO.id)}>
-                            <Typography.Link color='red'>
-                                Delete
-                            </Typography.Link>
-                        </Popconfirm>
-                    </Space>
-                )}
-            />
-        </Table>
+                            <Popconfirm
+                                title={postDetailVO.status === 'published' ? 'Revoke' : 'Publish'}
+                                onConfirm={() => updatePostStatusDetailAPI(postDetailVO.id, postDetailVO.status === 'published' ? PostStatusEnum.DRAFT : PostStatusEnum.PUBLISHED)}
+                            >
+                                <Typography.Link color='red'>
+                                    {postDetailVO.status === 'published' ? 'Revoke' : 'Publish'}
+                                </Typography.Link>
+                            </Popconfirm>
+                            <Popconfirm title="Sure to Delete?" onConfirm={() => deletePostAPI(postDetailVO.id)}>
+                                <Typography.Link color='red'>
+                                    Delete
+                                </Typography.Link>
+                            </Popconfirm>
+                        </Space>
+                    )}
+                />
+            </Table>
+        </div>
     )
 }
 
