44f0297bf083c8d94345a4140fb0186f9e4fa632
wwr <me@wwr.icu>
2026-04-03 03:12:14 +0800

    encapsulate fetch instead of axios

--- src/api/manage.ts
+++ src/api/manage.ts
@@ -1,37 +1,36 @@
-import {AxiosResponse} from "axios"
 import {ConfigRO, TrashBinRO, UserRO} from "@/model/request.ts"
 import {ConfigKeyEnum, DatabaseActionEnum} from "@/model/enum.ts"
-import {myAxios} from "@/common.ts"
+import {http} from "@/common.ts"
 import {TrashBinVO} from "@/model/response.ts"
 
 export const databaseAPI = async (action: DatabaseActionEnum) => {
-    return await myAxios.get(`/manage/database?action=${action}`).then(() => {})
+    return await http.get(`/manage/database?action=${action}`).then(() => {})
 }
 
 export const setConfigAPI = async (config: ConfigRO) => {
-    return await myAxios.post('/manage/config/set', config).then(() => {})
+    return await http.post('/manage/config/set', config).then(() => {})
 }
 
 export const getConfigAPI = async (key: ConfigKeyEnum) => {
-    return await myAxios.get(`/manage/config/get?key=${key}`).then((res: AxiosResponse<string | null>) => res.data)
+    return await http.get<string | null>(`/manage/config/get?key=${key}`)
 }
 
 export const userAPI = async (userRO: UserRO) => {
-    return await myAxios.post('/manage/user', userRO).then(() => {})
+    return await http.post('/manage/user', userRO).then(() => {})
 }
 
 export const trashGetAllAPI = async () => {
-    return await myAxios.get('/manage/trash/all').then((res: AxiosResponse<TrashBinVO[]>) => res.data)
+    return await http.get<TrashBinVO[]>('/manage/trash/all')
 }
 
 export const trashEditAPI = async (trashBinRO: TrashBinRO) => {
-    return await myAxios.post('/manage/trash/edit', trashBinRO).then(() => {})
+    return await http.post('/manage/trash/edit', trashBinRO).then(() => {})
 }
 
 export const totpEnforceAPI = async (enforce: boolean) => {
-    return await myAxios.get(`/manage/totp/enforce?enforce=${enforce}`).then((res: AxiosResponse<string | null>) => res.data)
+    return await http.get<string | null>(`/manage/totp/enforce?enforce=${enforce}`)
 }
 
 export const totpConfirmAPI = async (totp: string) => {
-    return await myAxios.get(`/manage/totp/confirm?totp=${totp}`).then(() => {})
+    return await http.get(`/manage/totp/confirm?totp=${totp}`).then(() => {})
 }
